68ec871c1e58d405d96fb78bec5ab954d3162fb2
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-31 14:58:46 +0100

    Add OOP-four principles

---
+++ dog.rb
@@ -0,0 +1,19 @@
+require "./animal.rb"
+require "./foods.rb"
+
+class Dog < Animal
+  def initialize(color, name = "unknown")
+    super("dog", 4, name)
+    @color = color
+    @liked_food = DogFood.new()
+  end
+
+  def bring_a_stick
+    "Here is your stick : ----"
+  end
+
+  def speak
+    "Woof, woof"
+  end
+
+end
---
+++ spider.rb
@@ -0,0 +1,19 @@
+require "./animal.rb"
+require "./foods.rb"
+
+class Spider < Animal
+  def initialize(web_strength_level, name = "unknown")
+    super("spider", 8, name)
+    @web_strength_level = web_strength_level
+    @liked_food = SpiderFood.new()
+  end
+
+  def make_a_web
+    "www"
+  end
+
+  def speak
+    "..."
+  end
+
+end
