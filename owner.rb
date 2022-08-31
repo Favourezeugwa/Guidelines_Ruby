190dd285a4aebd0abcf99822cce9dfd58a6fd758
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-31 15:38:24 +0100

    Add owner class

---
+++ owner.rb
@@ -0,0 +1,39 @@
+class Owner
+  attr_accessor :name
+  attr_reader :animals
+
+  def initialize(name)
+    @name = name
+    @animals = []
+  end
+
+  def add_animal(animal)
+    @animals.push(animal)
+  end
+end
+
+
+
+
+# TRY IT - CONSOLE
+require "./animal.rb"
+require "./dog.rb"
+require "./spider.rb"
+require "./owner.rb"
+
+dog = Dog.new("black", "Rax")
+spider = Spider.new(85, "Bob")
+animal = Animal.new("lion", 4, "Some name")
+
+alex = Owner.new("Alex")
+alex.animals
+alex.add_animal(dog)
+alex.animals
+alex.add_animal(spider)
+alex.animals
+alex.add_animal(animal)
+alex.animals.map {|animal| animal.name}
+
+alex.animals.count
+alex.animals.first.name
+alex.animals.first.number_of_legs
