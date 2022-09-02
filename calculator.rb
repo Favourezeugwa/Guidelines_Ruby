b0ce5c6446d06584e910c9a8bae5adb445d3109b
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-02 01:28:32 +0100

    Add calculator user input

---
+++ calculator.rb
@@ -0,0 +1,23 @@
+class Calc
+  puts "Enter first number: "
+  num1 = gets.chomp().to_f
+
+  puts "Enter operator: "
+  operator = gets.chomp()
+
+  puts "Enter second number: "
+  num2 = gets.chomp().to_f
+
+  if operator === "+"
+  puts (num1 + num2)
+  elsif operator === "-"
+    puts (num1 - num2)
+  elsif operator === "*" 
+    puts (num1 * num2)
+  elsif operator === "/"
+    puts (num1 / num2)
+  else 
+    puts ("This is an invalid operator")
+  end
+end
+  
