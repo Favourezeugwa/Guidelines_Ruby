c92b0e253608036b2706bb9d178d3fc5b5cecb1f
Favourezeugwa <favourezeugwa@gmail.com>
2022-09-02 02:12:32 +0100

    Add guess limit

--- guess.rb
+++ guess.rb
@@ -1,9 +1,21 @@
 secret_word = "fantasy"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_limit = false
 
-while guess != secret_word
-  puts "Enter a guess: "
-  guess = gets.chomp()
+while guess != secret_word and !out_of_limit
+  if guess_count < guess_limit
+    puts "Enter a guess: "
+    guess = gets.chomp()
+    guess_count += 1
+  else 
+    out_of_limit = true
+  end
 end
 
-puts "You Won!"
+if out_of_limit
+  puts "You Lose"
+else 
+  puts "Yipee, You won!"
+end
